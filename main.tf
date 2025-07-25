273708c769e94a1e72f334573d969b29d0458220
Dennis Buehring <1625376+erSitzt@users.noreply.github.com>
2025-07-25 09:48:53 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "vsphere" {
+  vim_keep_alive = 30
+  user           = "test"
+  password       = "test"
+  vsphere_server = "test"
+  allow_unverified_ssl = true
+}
+
+module "servers" {
+  
+  source             = "./modules/servers"
+
+
+}
